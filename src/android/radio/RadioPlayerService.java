613d5daa4a98a7214883760f42cba7c706eec968
Сергей <bugsmafia@gmail.com>
2017-01-15 14:02:08 +0500

    q

--- src/android/radio/RadioPlayerService.java
+++ src/android/radio/RadioPlayerService.java
@@ -176,7 +176,9 @@ public class RadioPlayerService extends Service implements PlayerCallback {
      */
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-
+	console.log(intent);
+	console.log(flags);
+	console.log(startId);
         String action = intent.getAction();
 
         /**
