1ed4d2b090d1b2969c371fbb1dc9320b13b421c0
Сергей <bugsmafia@gmail.com>
2017-01-15 21:36:14 +0500

    1

--- src/android/radio/RadioPlayerService.java
+++ src/android/radio/RadioPlayerService.java
@@ -597,7 +597,7 @@ public class RadioPlayerService extends Service implements PlayerCallback {
 			notification.flags = Notification.FLAG_ONGOING_EVENT;
 			notification.flags = Notification.FLAG_ONGOING_EVENT;
 			notification.icon = R.drawable.default_art;
-			//startForeground(NOTIFICATION_ID, notification);
+			startForeground(NOTIFICATION_ID, notification);
 
         /**
          * Expanded notification
