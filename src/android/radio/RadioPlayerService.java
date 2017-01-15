62cca296b2c6521dd98a40fd91125ed04d7220ae
Сергей <bugsmafia@gmail.com>
2017-01-15 12:58:49 +0500

    1

--- src/android/radio/RadioPlayerService.java
+++ src/android/radio/RadioPlayerService.java
@@ -213,7 +213,7 @@ public class RadioPlayerService extends Service implements PlayerCallback {
 			//startActivity(intent);
 			//startActivity(new Intent(Intent.ACTION_VIEW ));
 			//Intent intent = new Intent(Intent.ACTION_VIEW, "");
-			//Intent i = new Intent(RadioPlayerService.this, radioradio.ru.RadioPlayerService.class);
+			Intent i = new Intent(FLAG_ACTIVITY_NEW_TASK.this, radioradio.ru.RadioPlayerService);
 			String data = "radioradio.ru.RadioRadio";
 			//Intent intent = new Intent(Intent.ACTION_VIEW, data);
 			startActivity(intent);
